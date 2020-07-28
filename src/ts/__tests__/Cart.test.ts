6ad3921f1a85148675ae264978d11f8baea3f5d3
Alexandr Varygin <otz-job@mail.ru>
2020-07-28 20:11:29 +0300

    initial commit

---
+++ src/ts/__tests__/Cart.test.ts
@@ -0,0 +1,19 @@
+import Cart from '../service/Cart';
+import Book from '../domain/Book';
+import MusicAlbum from '../domain/MusicAlbum';
+import Movie from '../domain/Movie';
+
+test('new card should be empty', () => {
+  const cart = new Cart();
+
+  expect(cart.items.length).toBe(0);
+});
+
+test('add new Book, MusicALbum, Movie -> success', () => {
+  const cart = new Cart();
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new Movie(2000, 'Avengers', 'Joss Whedon', 1000, 137));
+
+  expect(cart.items.length).toBe(3);
+});
